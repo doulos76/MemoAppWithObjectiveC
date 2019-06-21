4d6e77e85263e5bbeda6d458b8875640d3c97461
doulos76 <inojyes@gmail.com>
2019-06-22 08:01:21 +0900

    implement memo model file

---
+++ MemoAppWithObjC/MemoAppWithObjC/Model/Memo.h
@@ -0,0 +1,24 @@
+//
+//  Memo.h
+//  MemoAppWithObjC
+//
+//  Created by dave76 on 22/06/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Memo : NSObject
+
+@property (strong, nonatomic) NSString* content;
+@property (strong, nonatomic) NSDate* insertDate;
+
+- (instancetype)initWithContent: (NSString*)content;
+
++ (NSArray*)dummyMemoList;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MemoAppWithObjC/MemoAppWithObjC/Model/Memo.m
@@ -0,0 +1,29 @@
+//
+//  Memo.m
+//  MemoAppWithObjC
+//
+//  Created by dave76 on 22/06/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+#import "Memo.h"
+
+@implementation Memo
+
+- (instancetype)initWithContent: (NSString*)content {
+  self = [super init];
+  if (self != nil) {
+    _content = content;
+  }
+  return self;
+}
+
++ (NSArray*)dummyMemoList {
+  Memo* memo1 = [[Memo alloc] initWithContent:@"Lorem Ipsum 111"];
+  Memo* memo2 = [[Memo alloc] initWithContent:@"Lorem Ipsum 111"];
+  
+  return [NSArray arrayWithObjects:memo1, memo2, nil];
+}
+
+
+@end
