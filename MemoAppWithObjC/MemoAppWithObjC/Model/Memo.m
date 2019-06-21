4646a685b3047cc66e2ebc8816fff132a71eff1a
doulos76 <inojyes@gmail.com>
2019-06-22 08:06:15 +0900

    design table view cell

--- MemoAppWithObjC/MemoAppWithObjC/Model/Memo.m
+++ MemoAppWithObjC/MemoAppWithObjC/Model/Memo.m
@@ -20,7 +20,7 @@
 
 + (NSArray*)dummyMemoList {
   Memo* memo1 = [[Memo alloc] initWithContent:@"Lorem Ipsum 111"];
-  Memo* memo2 = [[Memo alloc] initWithContent:@"Lorem Ipsum 111"];
+  Memo* memo2 = [[Memo alloc] initWithContent:@"Lorem Ipsum 222"];
   
   return [NSArray arrayWithObjects:memo1, memo2, nil];
 }
