40d9628dcc8bad28b7f49761655af06d8c913be5
doulos76 <inojyes@gmail.com>
2019-06-22 10:02:42 +0900

    add detail view controller

---
+++ MemoAppWithObjC/MemoAppWithObjC/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  MemoAppWithObjC
+//
+//  Created by dave76 on 22/06/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
