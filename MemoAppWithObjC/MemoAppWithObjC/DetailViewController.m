dd95085d60e0df3f9ae5b41553cdb435c14ba0eb
doulos76 <inojyes@gmail.com>
2019-06-22 10:29:10 +0900

    implement date cell

--- MemoAppWithObjC/MemoAppWithObjC/DetailViewController.m
+++ MemoAppWithObjC/MemoAppWithObjC/DetailViewController.m
@@ -10,6 +10,8 @@
 
 @interface DetailViewController () <UITableViewDataSource>
 
+@property (strong, nonatomic) NSDateFormatter* formatter;
+
 @end
 
 @implementation DetailViewController
@@ -21,9 +23,11 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
   if (indexPath.row == 0) {
     UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"memoCell" forIndexPath:indexPath];
+    cell.textLabel.text = self.memo.content;
     return cell;
   } else if (indexPath.row == 1) {
     UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"dateCell" forIndexPath:indexPath];
+    cell.textLabel.text = [self.formatter stringFromDate:self.memo.insertDate];
     return cell;
   }
   return [[UITableViewCell alloc] init];
@@ -31,18 +35,22 @@
 
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+  self.formatter = [[NSDateFormatter alloc] init];
+  self.formatter.dateStyle = NSDateFormatterLongStyle;
+  self.formatter.timeStyle = NSDateFormatterMediumStyle;
+  self.formatter.locale = [NSLocale localeWithLocaleIdentifier:@"Ko_kr"];
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
