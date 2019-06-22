ff00ea5a1f6ed0f68d4677bae482ad763f82be99
doulos76 <inojyes@gmail.com>
2019-06-22 09:24:24 +0900

    add Compose View Controller

---
+++ MemoAppWithObjC/MemoAppWithObjC/ComposeViewController.h
@@ -0,0 +1,17 @@
+//
+//  ComposeViewController.h
+//  MemoAppWithObjC
+//
+//  Created by dave76 on 22/06/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MemoAppWithObjC/MemoAppWithObjC/ComposeViewController.m
@@ -0,0 +1,32 @@
+//
+//  ComposeViewController.m
+//  MemoAppWithObjC
+//
+//  Created by dave76 on 22/06/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
